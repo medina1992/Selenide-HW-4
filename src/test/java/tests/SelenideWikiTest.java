a82e88fa7e7e4bb9475df647dcfa7780857d3b3d
Medina_Akhundova <medina.mstudio@gmail.com>
2025-04-16 23:30:17 +0300

    add changes

--- src/test/java/tests/SelenideWikiTest.java
+++ src/test/java/tests/SelenideWikiTest.java
@@ -4,23 +4,53 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byXpath;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+
 
 public class SelenideWikiTest {
 
     @BeforeAll
-    static void setUp() {
+    static void configureBrowser() {
         Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        baseUrl = "https://github.com/";
+        Configuration.holdBrowserOpen = true;
+
+
     }
 
     @Test
-    void checkJUnit5ExampleInSoftAssertions() {
-        open("https://github.com/selenide/selenide");
-
-        $("a[href$='/selenide/wiki']").click();
-        $("button.js-wiki-more-pages-link").click();
+    void checkJUnit5ExampleInSoftAssertionsTest() {
+        open(baseUrl + "/selenide/selenide");
+        $("#wiki-tab").click();
+        $(byXpath("//button[contains(text(), 'Show 3 more pages')]")).click();
         $$("a.Truncate-text").findBy(text("SoftAssertions")).click();
-        $$("span.pl-c1").findBy(text("ExtendWith")).shouldBe(visible);
+
+        $$("div.highlight-source-java pre")
+                .findBy(text("@ExtendWith({SoftAssertsExtension.class})"))
+                .shouldHave(
+                        text("Configuration.assertionMode = SOFT"),
+                        text("open(\"page.html\")"),
+                        text("$(\"#first\").should(visible).click()"),
+                        text("$(\"#second\").should(visible).click()")
+                );
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
